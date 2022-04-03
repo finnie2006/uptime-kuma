8c374abaddf407af94ce4ac4f05d583e997b42c2
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-04-03 20:11:08 +0200

    Update incident.js

--- server/model/incident.js
+++ server/model/incident.js
@@ -1,18 +1,33 @@
 const { BeanModel } = require("redbean-node/dist/bean-model");
-
 class Incident extends BeanModel {
-
     toPublicJSON() {
         return {
             id: this.id,
             style: this.style,
             title: this.title,
-            content: this.content,
-            pin: this.pin,
+            description: this.description,
+            overrideStatus: this.overrideStatus,
+            status: this.status,
             createdDate: this.createdDate,
-            lastUpdatedDate: this.lastUpdatedDate,
+            resolved: this.resolved,
+            resolvedDate: this.resolvedDate,
+        };
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            type: this.type,
+            style: this.style,
+            title: this.title,
+            description: this.description,
+            overrideStatus: this.overrideStatus,
+            status: this.status,
+            createdDate: this.createdDate,
+            parentIncident: this.parentIncident,
+            resolved: this.resolved,
+            resolvedDate: this.resolvedDate,
         };
     }
 }
-
 module.exports = Incident;
